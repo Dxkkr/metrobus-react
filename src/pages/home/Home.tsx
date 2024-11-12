5cc0a633218f8b7c03501161915ecd09ba53e4aa
Ryan <darwinxkk@gmail.com>
2024-11-12 04:46:22 -0300

    organização dos códigos Home.tsx e Home.css melhoradas

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,62 +1,50 @@
-// Home.tsx
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../client/supabaseClient";
 import { User } from "@supabase/supabase-js";
+import '@splidejs/splide/dist/css/splide.min.css';
 import '../../styles/Home.css';
 
 const Home: React.FC = () => {
   const [, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
 
+  const fetchUser = async () => {
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data.user) {
+      navigate("/login");
+    } else {
+      setUser(data.user);
+    }
+  };
+
   useEffect(() => {
-    const fetchUser = async () => {
-      const { data, error } = await supabase.auth.getUser();
-      if (error || !data.user) {
-        navigate("/login");
-      } else {
-        setUser(data.user);
-      }
-    };
     fetchUser();
   }, [navigate]);
 
   return (
-    <>
-      <section className="Section1">
-        {/* Introdução */}
-        <div className="headder">
-          <div className="headder-general">
-            {/* Título */}
-            <h1 className="Logo">MetroBus</h1>
-            <div className="frase">
-              <h2 id="text-1">Pontualidade</h2>
-              <h2 id="text-2">é nossa prioridade</h2>
-            </div>
-            {/* Fim do Título */}
-
-            {/* Slider */}
-              <div className="container">
-                <div className="logo">
-                  <img src="" alt="" />
-                </div>
-              </div>
-
-
-            {/* Fim do Slider */}
-          </div>
+    <section className="Section1">
+      {/* Header */}
+      <div className="Logo">
+        <h1>MetroBus</h1>
+      </div>
+      
+        <div className="frase">
+          <h2 id="text-1">Pontualidade</h2>
+          <h2 id="text-2">é nossa prioridade</h2>
         </div>
-        {/* Fim da Introdução */}
 
-        {/* D1 */}
-        <div className="middle">
-          <div className="change">
-            <h1 className="lalallal"></h1>
-          </div>
-        </div>
-        {/* Fim do D1 */}
-      </section>
-    </>
+      {/* Slider */}
+      {/* TODO: Implementar slider utilizando Splide */}
+
+      {/* Conteúdo de imagens */}
+      <div className="container-geral">
+        <div className="container">Imagem1</div>
+        <div className="container">Imagem2</div>
+        <div className="container">Imagem3</div>
+        <div className="container">Imagem4</div>
+      </div>
+    </section>
   );
 };
 
