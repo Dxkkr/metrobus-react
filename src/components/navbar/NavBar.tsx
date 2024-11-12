2f01c9d9a3964ed01bc9c4906f5bfac328d5e995
Ryan <darwinxkk@gmail.com>
2024-11-12 20:32:41 -0300

    NavBar Responsiva

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../../pages/home/Home";
 import Login from "../../pages/login/Login";
 import Signup from "../../pages/signup/Signup";
 // import Profile from "../../pages/profile/Profile"; // Página de perfil
 import '../../styles/NavBar.css'
+import { FiX } from "react-icons/fi";
+// import { FiAlignJustify } from "react-icons/fi";
+import { FiGrid } from "react-icons/fi";
+
+
 
 function NavBar() {
   return (
     <Router>
       <nav className="container-navigation">
         <ul className="container-link">
-          <li className="navitems"><h1>MetroBus</h1></li>
-        </ul>
+            <input type="checkbox" id="check"/>
+            <span className="menu">
+            <li className="Logo"><h1>MetroBus</h1></li>
+            <li className="cor"><a href="#">Horário de Ônibus</a></li>
+            <li className="cor"><a href="#">Recarga de Passe</a></li>
+            <li className="cor"><a href="#">Notificações</a></li>
+            <label htmlFor="check" className="close-menu"><FiX /></label>
+            </span>
+            <label htmlFor="check" className="open-menu"><FiGrid /></label>
 
-        {/* Botão para acessar o perfil */}
-        <Link to="/profile" className="navitems-2">
-          <button className="profile-button">User</button>
-        </Link>
+            
+        </ul>
       </nav>
 
       <Routes>
